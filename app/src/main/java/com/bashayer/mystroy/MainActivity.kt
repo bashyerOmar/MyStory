00e55bcd44260d9eba9e0f2812c246c0ce3183aa
bashyerOmar <besho.om.alghamdi@gmail.com>
2021-12-12 02:22:10 +0300

    write code for show email in the main activity

--- app/src/main/java/com/bashayer/mystroy/MainActivity.kt
+++ app/src/main/java/com/bashayer/mystroy/MainActivity.kt
@@ -2,10 +2,16 @@ package com.bashayer.mystroy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val email=intent.getStringExtra("email") //get entered email from the login activity
+        val tvemail:TextView = findViewById(R.id.tvEmail) // connect textview
+        tvemail?.text=email // assign email to the textview to show in the main activity
     }
+
 }
