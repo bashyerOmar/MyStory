92e8a56b72679246031c63e23f605dddc2beacda
bashyerOmar <besho.om.alghamdi@gmail.com>
2021-12-11 18:31:04 +0300

    create splash and login activites

---
+++ app/src/main/java/com/bashayer/mystroy/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.bashayer.mystroy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //delay the splash screen before move to the login screen
+        Handler(Looper.myLooper()!!).postDelayed({
+            finish() //to remove the splash activity from memory
+            //move to login screen
+            val theintent = Intent(this,LoginActivity::class.java)
+            startActivity(theintent)
+        },5000 )//5 seconds
+
+
+
+    }
+}
